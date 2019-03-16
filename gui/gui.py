51e6e42fd8adc20759a280633fa375dcc9845abf
nulano <nulano@nulano.eu>
2019-03-16 17:37:38 +0100

    gui: argparser

--- gui/gui.py
+++ gui/gui.py
@@ -1,3 +1,4 @@
+import argparse
 import tkinter as tk
 from xml.etree import ElementTree
 
@@ -7,7 +8,7 @@ from camera import Camera
 from renderer import Renderer
 
 if __name__ != '__main__':
-    raise AssertionError('gui can only be used as __main__ module')
+    raise AssertionError('gui can only be used as a __main__ module')
 
 
 def render(camera, renderer, panel):
@@ -19,19 +20,20 @@ def render(camera, renderer, panel):
     print('rendering done')
 
 
-#file = '../artists/testdata/lafranconi.osm'
-file = 'map.osm'
-dimensions = (800, 600)
+arg_parser = argparse.ArgumentParser(description='Gui for map project. Made by Nulano 2019.')
+arg_parser.add_argument('-f', '--map', default='map.osm', help='the OpenStreetMap xml file to use', dest='file')
+arg_parser.add_argument('--dimensions', default=(800, 600), nargs=2, help='use this resolution at startup', metavar=('X', 'Y'))
+args = arg_parser.parse_args()
 
 print('-- parsing map')
-element_tree = ElementTree.parse(file)
+element_tree = ElementTree.parse(args.file)
 print('-- processing map')
-camera = Camera(dimensions=dimensions)
+camera = Camera(dimensions=args.dimensions)
 renderer = Renderer(camera, element_tree)
 print('-- map loaded')
 
 root = tk.Tk()
-root.geometry('x'.join([str(x) for x in dimensions]))
+root.geometry('x'.join([str(x) for x in args.dimensions]))
 panel = tk.Label(root)
 panel.pack(side='bottom', fill='both', expand='yes')
 render(camera, renderer, panel)

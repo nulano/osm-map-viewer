4587af4a76ead691f662903554951bac0ceb74a7
nulano <nulano@nulano.eu>
2019-03-16 21:57:48 +0100

    artists: fix water lines treated as polygons sometimes

--- artists/artist_water.py
+++ artists/artist_water.py
@@ -1,12 +1,12 @@
 from artists_util import ArtistArea, TagMatches, TagPresent
 
 
-class ArtistWater(ArtistArea):
+class ArtistWaterArea(ArtistArea):
     def __init__(self):
         super().__init__(fill='#ade')
         self.filter += TagMatches('natural', ('water', 'bay', 'spring', 'hot_spring', 'blowhole')) \
                    .Or(TagMatches('landuse', ('basin', 'reservoir'))) \
-                   .Or(TagPresent('waterway'))
+                   .Or(TagMatches('waterway', ('riverbank', )))
 
 
-_all = {'water': ArtistWater()}
+_all = {'water': ArtistWaterArea()}
